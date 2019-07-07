ff396132f074692f4fbe5a3f58753337a6cba1cb
blkgoose <alessio.biancone@gmail.com>
2019-07-07 13:04:53 +0200

    feat: Added auto install script

---
+++ bashball-install/main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source "optparse/optparse.sh"
+
+optparse_usage_header="[option...] file..."
+optparse.define\
+    long=release\
+    variable=release\
+    desc="bashball release to install"\
+    default="latest"
+
+optparse.define\
+    long=wget-opt\
+    variable=wgetopt\
+    desc="options for wget download"\
+    default="-q --show-progress"
+
+optparse.define\
+    long=directory\
+    variable=outdir\
+    desc="where to install bashball"\
+    default="/bin/bashball"
+
+. <(optparse.build)
+
+set -euo pipefail
+
+
+[[ $release == 'latest' ]] && {
+    dl_link="https://github.com/blkgoose/bashball/releases/latest/download/bashball"
+} || {
+    dl_link="https://github.com/blkgoose/bashball/releases/download/v${release}/bashball"
+}
+
+echo "starting download for bashball v${release} in ${outdir}"
+sudo wget ${wgetopt} ${dl_link} -O ${outdir}
